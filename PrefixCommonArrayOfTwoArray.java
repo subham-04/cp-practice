36bdc23cdaf5d25ee36a916ff208b19ee43deb51
Subham Bhadra <70264692+subham-04@users.noreply.github.com>
2023-07-31 20:56:23 +0530

    Add files via upload

---
+++ PrefixCommonArrayOfTwoArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int[] result = new int[A.length];
+        for(int i=0; i<A.length; i++){
+            boolean found= false;
+            for(int j=0; j<B.length; j++){
+                if(A[i]==B[j]){
+                    found=true;
+                }
+                if(found && j>=i){
+                    result[j]++;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
